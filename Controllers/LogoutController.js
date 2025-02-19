d87101c2241026f5cc40db384afbab255e607cbd
Dickson <dicksonkinyua@gmail.com>
2025-02-19 21:40:49 +0300

    logout fn fix

--- Controllers/LogoutController.js
+++ Controllers/LogoutController.js
@@ -1,5 +1,8 @@
 export const logout = async (req, res) => {
   const token = req.cookies?.token;
+  if(!token){
+    return res.status(400).json({error:"Authorization failed.No token"})
+  }
   try {
     res.cookie("token", "").json("Logout successful");
   } catch (error) {

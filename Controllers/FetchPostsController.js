c7a4159b02da05f3a4012ae269dcd35709e72e47
Dickson <dicksonkinyua@gmail.com>
2025-02-20 12:59:24 +0300

    handling userID while fetching

--- Controllers/FetchPostsController.js
+++ Controllers/FetchPostsController.js
@@ -1,18 +1,18 @@
 import PostModel from "../models/postModel.js";
+import mongoose from "mongoose";
 
 export const fetchPosts = async (req, res) => {
   try {
     const userId = req.user?.userId;
-    console.log(userId);
+    console.log(typeof userId);
 
     if (!userId) {
       return res
         .status(401)
         .json({ error: "Unauthorized: No user ID provided" });
     }
-
-    const posts = await PostModel.find({ author: userId }).lean();
-    console.log("Posts found:", posts); // Debugging
+    const objectId = new mongoose.Types.ObjectId(userId); // Convert to ObjectId
+    const posts = await PostModel.find({ author: objectId }).lean();
 
     if (!posts.length) {
       return res.status(404).json({ error: "No posts found" });
--- Controllers/NewPostController.js
+++ Controllers/NewPostController.js
@@ -3,6 +3,7 @@ import { missingInput } from "./missingInputChecker.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 
 export const createPost = async (req, res) => {
   const { todo, priority } = req.body;
@@ -17,7 +18,7 @@ export const createPost = async (req, res) => {
       todo,
       priority,
       completed: false,
-      author: userId,
+      author: new mongoose.Types.ObjectId(userId),
     });
     if (!newPost) {
       return res.status(401).json({ error: "could not create post" });
